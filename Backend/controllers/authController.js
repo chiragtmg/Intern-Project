888a308820260a74597171e870dfb2e4e41d8cc6
chiragtmg <chiragtmg456@gmail.com>
2026-01-22 14:16:16 +0545

    Creating JWT key

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 // Register new user
 export const registerUser = async (req, res) => {
@@ -33,7 +34,7 @@ export const registerUser = async (req, res) => {
 
 		res
 			.status(201)
-			.json({ message: "User registered successfully", userResponse });
+			.json({ message: "User registered successfully" });
 	} catch (error) {
 		res.status(500).json({ message: "Internal server error" });
 		console.error(error);
